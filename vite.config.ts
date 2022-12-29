dd41dbde46baa4c2c0cc3c85d914d27a01b59456
BIYA Paul <bpsmartdesign@hotmail.com>
2022-12-29 14:18:14 +0100

    chore: Update vite configs.

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "url"
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Gubukuh- Real Estate Landing Page",
+        short_name: "gubukuh",
+        description: "Real Estate landing page",
+        theme_color: "#3f86fa",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3333,
+  },
+})
